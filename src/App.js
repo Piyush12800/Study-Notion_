4f7abc12794b3510da2d6312b4eff808653f608f
Piyush12800 <12800piyush@gmail.com>
2023-08-12 20:57:19 +0530

    Backend Connection & Authentication Successful

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
+import Navbar from "./components/Common/Navbar";
+import Login from "./pages/Login";
+import Signup from './pages/Signup'
+import OpenRoute from "./components/core/Auth/OpenRoute";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+  <Navbar/>
   <Routes>
     <Route path="/" element= {<Home/>}/>
+    {/* Open Route - for Only Non Logged in User */}
+    <Route
+          path="login"
+          element={
+            <OpenRoute>
+
+              <Login />
+            </OpenRoute>
+            
+          }
+        />
+         <Route
+          path="signup"
+          element={
+            <OpenRoute>
+
+              <Signup/>
+            </OpenRoute>
+            
+          }
+        />
+         <Route
+          path="signup"
+          element={
+            <OpenRoute>
+
+              <Signup/>
+            </OpenRoute>
+            
+          }
+        />
+         <Route
+          path="forgetpassword"
+          element={
+            <OpenRoute>
+
+              <ForgotPassword/>
+            </OpenRoute>
+            
+          }
+        />
   </Routes>
   </div>
   );
