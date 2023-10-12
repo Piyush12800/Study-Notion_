b6f9574d32e7353b8d12420755d7a3943ec98223
Piyush Kushwah <94518170+Piyush12800@users.noreply.github.com>
2023-10-12 16:25:51 +0530

    Update Template.js

--- src/components/core/Auth/Template.js
+++ src/components/core/Auth/Template.js
@@ -20,16 +20,16 @@ const Template = ({ title, desc1, desc2, image, formtype }) => {
           <LoginForm  />
         )}
 
-        <div className="flex w-full items-center my-4 gap-x-2 ">
+        {/* <div className="flex w-full items-center my-4 gap-x-2 ">
           <div className="w-full h-[1px] bg-richblack-700"></div>
           <p className="text-richblack-700 capitalize font-medium leading-[1.375rem]">or</p>
           <div className="w-full h-[1px] bg-richblack-700"></div>
-        </div>
-        <button className="w-full flex justify-center items-center rounded-[8px] font-medium text-richblack-100 border
+        </div> */}
+        {/* <button className="w-full flex justify-center items-center rounded-[8px] font-medium text-richblack-100 border
          border-richblack-700 px-[12px] py-[8px] gap-x-2 mt-6 ">
         <FcGoogle/>
           <p className="">Sign Up with Google</p>
-        </button>
+        </button> */}
       </div>
 
       <div className="relative w-11/12 max-w-[450px]">
